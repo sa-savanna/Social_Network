96a06069472c35884c6e739a227a44f06ec2be97
sa-savanna <savanna.be.ua@gmail.com>
2020-10-16 11:44:31 +0200

    final

--- clients/src/components/post/Post.js
+++ clients/src/components/post/Post.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from "../layout/Spinner"
@@ -18,7 +18,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     return loading || post === null ? (
         <Spinner />
     ) : (
-            <>
+            <Fragment>
                 <Link to="/posts" className="btn">
                     Back To Posts
                 </Link>
@@ -29,7 +29,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
                         <CommentItem key={comment._id} comment={comment} postId={post._id} />
                     ))}
                 </div>
-            </>
+            </Fragment>
         )
 }
 
